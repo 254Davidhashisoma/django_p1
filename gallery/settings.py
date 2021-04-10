166d7e06a219baeb782ccee3adfa558e0fea19f1
254Davidhashisoma <davidhashisoma95@gmail.com>
2021-04-10 11:48:22 +0300

    urls

--- gallery/settings.py
+++ gallery/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
