6b57c4e54a064d4adee1045d0dad6a92c64151b0
254Davidhashisoma <davidhashisoma95@gmail.com>
2021-04-10 11:54:56 +0300

    debug views

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,27 @@
+
 from django.shortcuts import render
+from .models import Image, Location, Category
+
 
 # Create your views here.
+def photos(request):
+    photos = Image.objects.all()
+    locations = Location.objects.all()
+    return render(request,'photos.html',{'photos':photos,'locations':locations})
+
+def locations(request,location):
+    locations = Image.filterimageByLocation(location)
+    return render(request,'locations.html',{'locations':locations})
+
+def search_results(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_photos = Image.searchImage(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'search.html',{"message":message,"photos": searched_photos})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html',{"message":message})
