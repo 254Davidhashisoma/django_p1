d5c775737b20f3ed97b423fcb7c0b80e57d823ea
254Davidhashisoma <davidhashisoma95@gmail.com>
2021-04-10 09:51:57 +0300

    ading the app

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'cloudinary'
-    'photos'
+    'cloudinary',
+    'photos',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
